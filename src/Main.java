743f64c04cb33bbc773b90a98a427efa70642652
Azo Ismail <ismailazo260@gmail.com>
2026-01-06 00:41:52 +0300

    feat: Add Main class to demonstrate the Bridge design pattern.

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
         Controller controller = new KeyboardController();
+        Controller controller1 = new GamepadController();
         Game game = new ActionGame(controller);
 
+        Game game1 =new RacingGame(controller1);
         game.play();
+        game1.play();
 
     }
 }
